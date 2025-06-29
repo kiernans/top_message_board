297d810bdf2f20a1cffb31b54685d312326042cb
Kiernan Soriano <nukabunny@gmail.com>
2025-06-29 13:08:17 -0700

    Created ID creation function and allow for searching by message ID

--- db.js
+++ db.js
@@ -1,22 +1,32 @@
 const messages = [
     {
+        id: createNewId(),
         text: "Hi there!",
         user: "Amando",
         added: new Date()
     },
     {
+        id: createNewId(),
         text: "Hello World!",
         user: "Charles",
         added: new Date()
     }
 ];
 
+function createNewId() {
+    return Math.random().toString(16).slice(2);
+}
+
 async function getMessages() {
     return messages;
 };
 
 async function addMessage(user, text) {
-    messages.push({ text: text, user: user, added: new Date() });
+    messages.push({ id: createNewId(), text: text, user: user, added: new Date() });
 }
 
-module.exports = { getMessages, addMessage };
+async function getMessageById(id) {
+    return messages.find(message => message.id === id);
+}
+
+module.exports = { getMessages, addMessage, getMessageById };
