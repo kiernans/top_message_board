cd9e4b066f5394f3408b83472bfaa18017fd4e24
Kiernan Soriano <nukabunny@gmail.com>
2025-06-29 11:53:14 -0700

    Added in middleware for parsing request body

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//Set up parsing into req.body
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
